07aedbf4bbd82f896624f337c2f48f039817951c
Ritik Roshan Singh <ritikroshansingh06@Ritiks-MacBook-Air.local>
2026-03-10 12:30:52 +0530

    still working...

---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,21 @@
+import {createContext } from "react"
+import { food_list } from "../assets/frontend_assets/assets"
+
+
+export const StoreContext = createContext({
+  food_list: []
+})
+
+const StoreContextProvider = (props) => {
+
+    const contextValue = {
+     food_list
+
+}
+return (
+    <StoreContext.Provider value={contextValue}>
+        {props.children}
+    </StoreContext.Provider>
+)
+}
+export default StoreContextProvider
