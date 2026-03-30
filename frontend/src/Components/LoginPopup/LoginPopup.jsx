f73a6daa24698fce3b6972daf492a778479a1f2a
Ritik Roshan Singh <ritikroshansingh06@gmail.com>
2026-03-31 00:31:24 +0530

    add login and signup

--- frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/frontend_assets/assets'
 
-const LoginPopuo = () => {
+const LoginPopup = ({setShowLogin}) => {
+   const [currentState, setCurrentState] = useState("sign up")
+
   return (
-    <div>LoginPopuo</div>
-  )
+    <div className="login-popup"> 
+    <form  className="login-popup-container">
+      <div className="login-poup-title">
+        <h2>{currentState}</h2>
+        <img onClick={()=> setShowLogin(false)} src={assets.cross_icon} alt="" />
+
+      </div>
+      <div className="login-popup-inputs">
+        <input type="text"  placeholder='Your name ' required />
+        <input type="email"  placeholder='Your email ' required />
+        <input type='password' placeholder='' required />
+      </div>
+      <button>{currentState === "sign up" ? "Create Account" : "Login"}</button>
+
+
+    </form>
+        
+</div>  )
 }
 
-export default LoginPopuo
+export default LoginPopup
