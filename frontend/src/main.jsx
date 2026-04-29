98e84a3b255533f0ae0d0e52478412253582c333
Ritik Roshan Singh <ritikroshansingh06@gmail.com>
2026-04-30 01:55:27 +0530

    fix: case-sensitive path issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
+import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
